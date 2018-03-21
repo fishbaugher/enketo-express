c226ffbf3e58b49081d5a995c6c337468185a1e1
Martijn van de Rijdt <martijn@enketo.org>
2018-03-21 13:55:59 -0600

    converted from async/await to old nodeJS 6 friendly promises

--- app/lib/pdf.js
+++ app/lib/pdf.js
@@ -5,13 +5,11 @@ const MARGIN = '0.5in';
 const LANDSCAPE = false;
 const SCALE = 1;
 
-async function get( url, options = {} ) {
+/*async*/
+function get( url, options = {} ) {
     if ( !url ) {
         throw new Error( 'No url provided' );
     }
-    console.time( 'total pdf' );
-    const browser = await puppeteer.launch( { headless: true } );
-    const page = await browser.newPage();
 
     options.format = options.format || FORMAT;
     options.margin = options.margin || MARGIN;
@@ -24,29 +22,41 @@ async function get( url, options = {} ) {
     urlObj.searchParams.append( 'landscape', options.landscape );
     urlObj.searchParams.append( 'scale', options.scale );
 
-    console.log( 'url to pdf', urlObj.href );
-    console.time( 'page load' );
-    await page.goto( urlObj.href, { waitUntil: 'networkidle0' } );
-    await page.waitForFunction( 'window.printReady === true', { polling: 200 } );
-    console.timeEnd( 'page load' );
-    console.time( 'pdf generation' );
-    const pdf = page.pdf( {
-        landscape: options.landscape,
-        format: options.format,
-        margin: {
-            top: options.margin,
-            left: options.margin,
-            right: options.margin,
-            bottom: options.margin
-        },
-        scale: options.scale
-    } );
-    await pdf;
-    console.timeEnd( 'pdf generation' );
-    await browser.close();
-    console.log( 'browser closed' );
-    console.timeEnd( 'total pdf' );
-    return pdf;
+    //const browser = await puppeteer.launch( { headless: true } );
+    //const page = await browser.newPage();
+    //await page.goto( urlObj.href, { waitUntil: 'networkidle0' } );
+    //await page.waitForFunction( 'window.printReady === true', { polling: 200 } );
+    let browser;
+    let page;
+    return puppeteer.launch( { headless: true } )
+        .then( brows => {
+            browser = brows;
+            return browser.newPage();
+        } )
+        .then( pg => {
+            page = pg;
+            return page.goto( urlObj.href, { waitUntil: 'networkidle0' } );
+        } )
+        .then( () => {
+            return page.pdf( {
+                landscape: options.landscape,
+                format: options.format,
+                margin: {
+                    top: options.margin,
+                    left: options.margin,
+                    right: options.margin,
+                    bottom: options.margin
+                },
+                scale: options.scale
+            } );
+        } )
+        .then( pdf => {
+            browser.close(); // don't wait?
+            return pdf;
+        } );
+    //await pdf;
+    //await browser.close();
+    //return pdf;
 }
 
 module.exports = { get };
